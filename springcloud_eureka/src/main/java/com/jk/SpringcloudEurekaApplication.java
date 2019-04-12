8ab12f500625568e6496a45a52e02c62e95cea22
zhaoxiaodou <2356957840@qq.com>
2019-04-12 19:16:45 +0800

    初始化项目

---
+++ springcloud_eureka/src/main/java/com/jk/SpringcloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaApplication.class, args);
+    }
+
+}
